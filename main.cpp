b461398bbde02a32ad17e065f7e38056efb8a0ba
Exponenta <the.exponenta@gmail.com>
2025-04-16 06:03:29 -0400

    Add run syn scan from main

--- main.cpp
+++ main.cpp
@@ -2,14 +2,47 @@
 #include <sys/socket.h>
 #include <ctime>
 #include <fstream>
+#include <thread>
 #include <arpa/inet.h>
-#include "syn_scanner.h"
+#include "scanning/scanning.h"
+#include "scanning/syn_scanner.h"
+#include "random.h"
+#include "cli.h"
 
-#define WS_VERSION 0x0202
 
-
-int main() {
+int main(int argc, char** argv) {
     srand(time(nullptr));
+
+    ScanParams scanParams {};
+    scanParams.wait = 10;
+    randBytes(scanParams.seed, sizeof(ScanParams::seed));
+    getScanParams(scanParams, argc, argv);
+
+    ScanState scanState {scanParams};
+
+    synScan(scanState);
+
+    for (auto &host : scanState.result.hostsInfo()) {
+        char addrStr[32];
+        in_addr addr{};
+        addr.s_addr = htonl(host.ip);
+        inet_ntop(AF_INET, &addr, addrStr, sizeof(addrStr));
+
+        std::cout << addrStr << "\n";
+
+        for (const PortInfo &portInfo : host.portsInfo) {
+            std::cout << portInfo.port << " ";
+
+            if (portInfo.state == PortState::OPEN)
+                std::cout << "OPEN";
+            else
+                std::cout << "CLOSED";
+
+            std::cout << '\n';
+        }
+
+        std::cout << '\n';
+    }
 
     return 0;
 }
